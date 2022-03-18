a7c0a3b8b8e33aae766cabb3924b7a36f4cdcfb4
dishantMHAKHARIYA <99314153+dishantMHAKHARIYA@users.noreply.github.com>
2022-03-19 02:24:14 +0530

    quizqn

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following country is recently put on the “grey list” by Financial Action Task Force (FATF)?",
+    answer: "United Arab Emirates",
+    options: [
+      "Oman",
+      "Vietnam",
+      "South Korea",
+      "United Arab Emirates"
+    ]
+  },
+    {
+    numb: 2,
+    question: "In February 2022, there was a massive protest in Canada against",
+    answer: "Covid Vaccine",
+    options: [
+      "inflation",
+      "taxation",
+      "pension deduction",
+      "Covid Vaccine"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who has recently donated $5.7 billion to a charity?",
+    answer: "Elon Musk",
+    options: [
+      "Bill Gates",
+      "Elon Musk",
+      "Larry Page",
+      "Mark Zukerberg"
+    ]
+  },
+    {
+    numb: 4,
+    question: "The Donbas War is currently being fought in",
+    answer: "Ukraine",
+    options: [
+      "Ukraine",
+      "Syria",
+      "Serbia",
+      "Lebanon"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Some parts of South-East Ukraine are currently occupied by rebels backed by",
+    answer: "Russia",
+    options: [
+      "Serbia",
+      "Israel",
+      "Russia",
+      "United States"
+    ]
+  },
+  
+];
